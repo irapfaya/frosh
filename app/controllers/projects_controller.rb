8211831f92aac2c28ed50a85f81681e25ff9ad61
irapfaya <arindomudiare@gmail.com>
2016-09-09 09:46:02 +0000

    Redirect the user back to the project page with an error message if the click a non existent link

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
   def index
     @projects = Project.all
   end
@@ -21,22 +22,22 @@ class ProjectsController < ApplicationController
 
 
   def show
-    @project = Project.find(params[:id])
+    #@project = Project.find(params[:id])
 
   end
 
   def edit
-    @project = Project.find(params[:id])
+    #@project = Project.find(params[:id])
   end
   def destroy
-    @project = Project.find(params[:id])
+    #@project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted"
     redirect_to projects_path
   end
 
   def update
-    @project = Project.find(params[:id])
+    #@project = Project.find(params[:id])
     @project.update(project_params)
 
     flash[:notice] = "Project has been updated"
@@ -54,6 +55,13 @@ class ProjectsController < ApplicationController
   def project_params
     params.require(:project).permit(:name, :description)
 
+  end
+
+  def set_project
+     @project = Project.find(params[:id])
+     rescue ActiveRecord::RecordNotFound
+     flash[:alert] = "The project you were looking for could not be found."
+     redirect_to projects_path
   end
 
 
