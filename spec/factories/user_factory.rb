430ac3b5448ad981dc9336b5aae5789953fd1c4d
irapfaya <arindomudiare@gmail.com>
2016-09-13 12:21:36 +0000

    Added admin flag to User model, and seeded the first admin user without testing

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,9 +1,15 @@
 FactoryGirl.define do
 
 	factory :user_factory do
-
+    
 		sequence(:email) { |n| "test #{n} @example.com" }
 		password "password"
+
+		trait :admin do
+
+		  admin true
+
+		end
 
 	end
 
